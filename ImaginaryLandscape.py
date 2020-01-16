e73a3f5fd2b473a48c5ed7360ee80bbb5c2f59f0
ewatts3 <ewatts3@ashland.edu>
2020-01-16 17:52:29 -0500

    1.0 - Version when I started using Git

---
+++ ImaginaryLandscape.py
@@ -0,0 +1,93 @@
+import pyaudio
+import wave
+import os
+import random
+import threading
+from time import time, sleep
+
+class ImaginaryLandscape:
+	def __init__(self):
+		self.startTime = time() #time will be used to make descisons at various points in the program
+		self.done = False #determines if program will stop after x-minutes
+
+		self.makeThreads()
+		self.readInAudioFiles()
+		self.perform()
+
+	#creates an array of threads that will be use to play the sound files
+	def makeThreads(self):
+		self.threads = []
+		t1 = threading.Thread()
+		t2 = threading.Thread()
+		t3 = threading.Thread()
+		self.threads.append(t1)
+		self.threads.append(t2)
+		self.threads.append(t3)
+
+	#place all the audio files in an array to be used later
+	def readInAudioFiles(self):
+		self.audioFiles = []
+		count = 0
+		for entry in os.scandir(): 
+			if(".wav" in entry.name):
+   				self.audioFiles.append(entry)
+   				print(self.audioFiles[count].name)
+   				#print(os.path.realpath(self.audioFiles[count]))
+   				count = count + 1
+
+	def decideIfThreadShouldBeStarted(self, threadNumber):
+		if threadNumber == 0 and random.randrange(0, 2, 1) == 0 or threadNumber == 1 and random.randrange(0, 4, 1) == 0 or threadNumber == 2 and random.randrange(0, 9, 1) == 0:
+			self.startThread(threadNumber)
+
+	def startThread(self, threadNumber):
+		self.threads[threadNumber] = threading.Thread(target=self.play, 
+			args = (self.audioFiles[random.randrange(0, len(self.audioFiles) - 1, 1)].name,))
+		self.threads[threadNumber].start() 
+
+	def play(self, fileName):
+		chunk = 1024
+		f = wave.open(fileName, "rb")
+		p = pyaudio.PyAudio()
+		stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
+					input_device_index = 4,  
+               		channels = f.getnchannels(),  
+                	rate = f.getframerate(),  
+                	output = True)  
+		data = f.readframes(chunk)
+		i = 0
+		j = random.randrange(100, 1001, 1)
+		while data:
+			stream.write(data)
+			data = f.readframes(chunk)
+			i += 1
+			if i == j:
+				data = False
+
+	def perform(self):
+		i = 0
+		while not self.done:
+			print("begin") 
+			i += 1
+			if i == 180:
+				self.done = True
+
+			if ((self.threads[0].is_alive() is False) and (random.randrange(0, 2, 1) == 0)):
+				print("here0")
+				self.threads[0] = threading.Thread(target=self.play, 
+					args = (self.audioFiles[random.randrange(0, len(self.audioFiles) - 1, 1)].name,))
+				self.threads[0].start() 
+			if ((self.threads[1].is_alive() is False) and (random.randrange(0, 4, 1) == 0)):
+				print("here1")
+				self.threads[1] = threading.Thread(target=self.play, 
+					args = (self.audioFiles[random.randrange(0, len(self.audioFiles) - 1, 1)].name,))
+				self.threads[1].start() 
+			if ((self.threads[2].is_alive() is False) and (random.randrange(0, 9, 1) == 0)):
+				print("here2")
+				self.threads[2] = threading.Thread(target=self.play, 
+					args = (self.audioFiles[random.randrange(0, len(self.audioFiles) - 1, 1)].name,))
+				self.threads[2].start()
+
+			sleep(1) 
+
+il = ImaginaryLandscape()
+print("done")
