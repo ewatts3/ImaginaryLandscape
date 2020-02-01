d0fcf0a4f0655dccf730205e2cc73d34a4559328
ewatts3 <ewatts3@ashland.edu>
2020-01-31 22:59:44 -0500

    Refactoring
    Added "Music" Class
    Moved "play()" method to "Music" class
    Removed "try/except" from "play()"
    Added "setter" methods to "Parameters" class
    Added "decideIfPieceShouldEnd()" method to ImaginaryLandscape class

--- ImaginaryLandscape.py
+++ ImaginaryLandscape.py
@@ -11,6 +11,7 @@ class ImaginaryLandscape:
 		self.parameters = parameters #parameters set by the user
 		self.startTime = time() #time will be used to make descisons at various points in the program
 		self.done = False #determines if program will stop after x-minutes
+		self.m = Music() #public class to do operations relating to playing samples
 
 		self.makeThreads(self.parameters.getNumberOfTracks(), self.parameters.getDivisionConstant())
 		self.readInAudioFiles()
@@ -35,8 +36,16 @@ class ImaginaryLandscape:
 			if(".wav" in entry.name):
    				self.audioFiles.append(entry)
    				print(self.audioFiles[count].name)
-   				#print(os.path.realpath(self.audioFiles[count]))
    				count = count + 1
+
+   	def perform(self):
+		while not self.done:
+			print("beginLoop")
+
+			self.decideIfThreadShouldBeStarted(0)
+			self.sleep()
+			self.done = self.decideIfPieceShouldEnd()
+		return
 
    	#determines if each thread will start are not
    	#recusively calls subsequent threads
@@ -49,8 +58,10 @@ class ImaginaryLandscape:
 			and (random.randrange(0, 100, 1) < self.threads[index].getProbability()) ):
 			#start thread
 			self.threads[index].setThread(
-				threading.Thread(target=self.play, 
-				args = (self.selectRandomAudioFile(),))
+				threading.Thread(target=self.m.play, 
+				args = (self.selectRandomAudioFile(),
+					self.parameters.getMinimumLengthOfSample(),
+					self.parameters.getMaximumLengthOfSample(),))
 				)
 			self.threads[index].getThread().start()
 			print('start' + str(index))
@@ -67,7 +78,7 @@ class ImaginaryLandscape:
 		return
 
 	def selectRandomAudioFile(self):
-		return self.audioFiles[random.randrange(0, len(self.audioFiles) - 1, 1)].name
+		return self.audioFiles[random.randrange(0, len(self.audioFiles) - 1, 1)]
 
 	def sleep(self):
 		sleep(random.randrange(
@@ -76,65 +87,17 @@ class ImaginaryLandscape:
 			1)) 
 		return
 
-	def play(self, fileName):
-		try:
-			chunk = 1024
-			f = wave.open(fileName, "rb")
-			p = pyaudio.PyAudio()
-			stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
-						input_device_index = 4,  
-	               		channels = f.getnchannels(),  
-	                	rate = f.getframerate(),  
-	                	output = True)  
-			#data = f.readframes(chunk) 	does this do anything????
+	def decideIfPieceShouldEnd(self):
+		if (time() - self.startTime > self.parameters.getMinimumLengthOfPiece()):
+			if(time() - self.startTime > self.parameters.getMaximunLengthOfPiece()):
+				#implement kill all threads
+				self.done = True
+				return True
+			elif(random.randrange(0, 10, 1) == 0):
+				self.done = True
+				return True
 
-			#find length of audio file in seconds
-			frames = f.getnframes()
-			rate = f.getframerate()
-			duration = int(frames / float(rate))
-			#print(duration)
-
-			start = random.randrange(0, duration, 1)
-			length = random.randrange(self.parameters.getMinimumLengthOfSample(), 
-				self.parameters.getMaximumLengthOfSample(), 1)
-
-			#skip unwanted frames
-			n_frames = int(start * f.getframerate())
-			f.setpos(n_frames)
-
-			# write desired frames to audio buffer
-			n_frames = int(length * f.getframerate())
-			frames = f.readframes(n_frames)
-			stream.write(frames)
-
-			stream.close()
-			p.terminate()
-			f.close()
-			print("endTrack")
-			return
-		#I've been having an issue with sometimegetting an error 
-		#"OSError: [Errno -9996] Invalid output device (no default output device)"
-		#at line 75, so I've input this try/except so the program can continue
-		except:
-			print("Errno -9996")
-			return
-
-	def perform(self):
-		while not self.done:
-			print("beginLoop")
-
-			self.decideIfThreadShouldBeStarted(0)
-
-			if (time() - self.startTime > self.parameters.getMinimumLengthOfPiece()):
-				if(time() - self.startTime > self.parameters.getMaximunLengthOfPiece()):
-					#kill all threads
-					self.done = True
-					return
-				elif(random.randrange(0, 10, 1) == 0):
-					self.done = True
-					return
-
-			self.sleep()
+		return False
 
 #class of parameters set by the user
 class Parameters:
@@ -151,26 +114,58 @@ class Parameters:
 		self.minimumLengthOfSample = minimumLengthOfSample
 		self.maximumLengthOfSample = maximumLengthOfSample
 
+	def setNumberOfTracks(self, input):
+		self.numberOfTracks = input
+		return
+
 	def getNumberOfTracks(self):
 		return self.numberOfTracks
+
+	def setDivisionConstant(self, input):
+		self.divisionConstant = input
+		return
 
 	def getDivisionConstant(self):
 		return self.divisionConstant
 
+	def setMinimumTimeBetweenChecks(self, input):
+		self.minimumTimeBetweenChecks = input
+		return
+
 	def getMinimumTimeBetweenChecks(self):
 		return self.minimumTimeBetweenChecks
+
+	def setMaximumTimeBetweenChecks(self, input):
+		self.maximumTimeBetweenChecks = input
+		return
 
 	def getMaximumTimeBetweenChecks(self):
 		return self.maximumTimeBetweenChecks
 
+	def setMinimumLengthOfPiece(self, input):
+		self.minimunLengthOfPiece = input
+		return
+
 	def getMinimumLengthOfPiece(self):
 		return self.minimunLengthOfPiece
+
+	def setMaximumLengthOfPiece(self, input):
+		self.maximumLengthOfPiece = input
+		return
 
 	def getMaximunLengthOfPiece(self):
 		return self.maximumLengthOfPiece
 
+	def setMinimumLengthOfSample(self, input):
+		self.minimumLengthOfSample = input
+		return
+
 	def getMinimumLengthOfSample(self):
 		return self.minimumLengthOfSample
+
+	def getMaximumLengthOfSample(self, input):
+		self.maximumLengthOfSample = input
+		return
 
 	def getMaximumLengthOfSample(self):
 		return self.maximumLengthOfSample
@@ -180,7 +175,7 @@ class Thread:
 	def __init__(self, thread, probability):
 		self.thread = thread
 		self.probability = int(probability)
-		print(self.probability)
+		#print(self.probability)
 
 	def getThread(self):
 		return self.thread
@@ -190,6 +185,54 @@ class Thread:
 
 	def setThread(self, threadStart):
 		self.thread = threadStart
+
+#public class to do all the operations relating to playing the file
+class Music:
+	#performs all necessary operations to play a file
+	def play(self, musicFile, min, max):
+		file = wave.open(musicFile.name, "rb")
+		p = pyaudio.PyAudio()
+		stream = self.openStream(p, file)
+
+		durationOfTrack = self.getDuration(file)
+		start = self.decideStart(durationOfTrack)
+		length = self.decideLength(min, max)
+		file.setpos(self.setPosition(start, file))
+		stream.write(self.getFramesToWrite(length, file))
+
+		stream.close()
+		p.terminate()
+		file.close()
+		print("endTrack")
+		return
+
+	#open audio output stream
+	def openStream(self, pyaudio, file):
+		return pyaudio.open(format = pyaudio.get_format_from_width(file.getsampwidth()),
+					input_device_index = 4,  
+	               	channels = file.getnchannels(),  
+	                rate = file.getframerate(),  
+	                output = True)  
+
+	#find length of audio file in seconds
+	def getDuration(self, file):
+		return int(file.getnframes() / float(file.getframerate()))
+
+	#decide starting position of sample
+	def decideStart(self, duration):
+		return random.randrange(0, duration, 1)
+
+	#decide total length of sample
+	def decideLength(self, min, max):
+		return random.randrange(min, max, 1)
+
+	#set position of open audio file to correct starting point
+	def setPosition(self, start, file):
+		return int(start * file.getframerate())
+
+	#calculate which frames to write to the audio buffer
+	def getFramesToWrite(self, length, file):
+		return file.readframes(int(length * file.getframerate()))
 
 #maximum number of tracks that can be playing at one time
 numberOfTracks = 3
@@ -202,13 +245,13 @@ divisionConstant = 2
 #for the next thread, we take that 25, and divide it by 2, and get 12 (through integer division)
 #etc...
 
-#time between each check for if the program should play a thread or not
+#time in seconds between each check for if the program should play a thread or not
 minimumTimeBetweenChecks = 1
-maximumTimeBetweenChecks = 5
-#length of the the whole performance
+maximumTimeBetweenChecks = 2
+#length of the the whole performance in seconds
 minimunLengthOfPiece = 60 * (1)
 maximumLengthOfPiece = 60 * (1)
-#range of length for each individual sample
+#range of length for each individual sample in seconds
 minimumLengthOfSample = 0
 maximumLengthOfSample = 15
 
